3929b0905b310bd7b18f72cf9f545a8effe1e34b
Etienne Soulard-Geoffrion <50806019+etiennecl@users.noreply.github.com>
2025-01-29 15:53:24 -0500

    feat(go): [RET-2184] chunker (#5)
    * cleanup dead code
    
    * feat: implement chunker interface and its implementation in Go client
    
    * feat: support for request id in infer method
    
    * feat: chunker and refactor common types
    
    * mkae method public

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { EmbedderClient } from "./embedder_client";
-export { TritonClient } from "./triton_client";
-export { RankerClient } from "./ranker_client";
-export { SummarizerClient, decodeTextFromBytes } from "./summarizer_client";
